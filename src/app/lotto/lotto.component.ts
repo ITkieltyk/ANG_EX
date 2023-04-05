56fb3e4fc955a7d8d3820d5ff77eb527bc4a03a2
ITkieltyk <kieltyk+gh@gmail.com>
2023-04-06 00:22:15 +0200

    lotto with typewritter effect

--- src/app/lotto/lotto.component.ts
+++ src/app/lotto/lotto.component.ts
@@ -8,26 +8,38 @@ import { Component, OnInit } from '@angular/core';
 export class LottoComponent implements OnInit {
   maszynaLosujaca: number[] = [];
   tekst1linijka: string = '';
-
+  wonNumbers: number[] = [];
   constructor() {}
 
   ngOnInit(): void {}
-  losowanie(element: HTMLElement) {
-    const tekst1 = 'Maszyna losująca jest pusta';
-    for (let i = 0; i < tekst1.length; i++) {
-      setTimeout(() => {
-        this.tekst1linijka = tekst1.slice(0, i + 1);
-      }, 100 * i);
-    }
 
-    const tekst2 = 'Następuje zwolnienie blokady';
-    for (let i = 0; i < tekst2.length; i++) {
+  losowanie(element: HTMLElement) {
+    this.tekst1linijka = '';
+    const tekst1 = 'Maszyna losująca jest pusta';
+    this.tekst1linijka = tekst1;
+    element.classList.remove('typed-out3');
+    element.classList.add('typed-out');
+    setTimeout(() => {
+      const tekst2 = 'Następuje zwolnienie blokady';
+      this.tekst1linijka = tekst2;
+      element.classList.toggle('typed-out');
+
+      element.classList.toggle('typed-out2');
       setTimeout(() => {
-        this.tekst1linijka = tekst2.slice(0, i + 1);
-      }, 100 * i + 5000);
-    }
-    for (let i = 0; i < 49; i++) {
-      this.maszynaLosujaca.push(i);
-    }
+        const tekst3 = 'Wylosowane liczby to:';
+        for (let i = 1; i < 50; i++) {
+          this.maszynaLosujaca.push(i);
+        }
+        for (let i = 0; i < 6; i++) {
+          let wonIndex: number = Math.floor(Math.random() * (49 - i));
+          this.wonNumbers.push(this.maszynaLosujaca[wonIndex]);
+          this.maszynaLosujaca.splice(wonIndex, 1);
+        }
+        this.tekst1linijka = tekst3 + ' ' + this.wonNumbers.join(', ');
+        element.classList.toggle('typed-out2');
+
+        element.classList.toggle('typed-out3');
+      }, 4000);
+    }, 5000);
   }
 }
